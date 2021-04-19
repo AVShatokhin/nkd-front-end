91560c918485dc1329336d44469903755ee59892
root <root@nkd.gtlab.pro>
2021-04-19 09:17:47 +0300

    Правки скриптов додкера

--- dockers/my.win.bat
+++ dockers/my.win.bat
@@ -1,2 +1,3 @@
+docker stop nkd-db
 docker rm nkd-db
 docker run -it -d -e MYSQL_ROOT_PASSWORD=13454678qwerty -v d:/projects/nkd-my-database-lib/:/var/lib/mysql -v d:/projects/nkd-my-database-config/:/etc/mysql/conf.d -p 3306:3306 --name nkd-db mariadb:10
--- dockers/phpmyadmin.cross.bat
+++ dockers/phpmyadmin.cross.bat
@@ -1,2 +1,3 @@
+docker stop nkd-phpmyadmin
 docker rm nkd-phpmyadmin
 docker run -it -d -e MYSQL_ROOT_PASSWORD=13454678qwerty -e PMA_HOST=nkd-db --link nkd-db -p 8080:80 --name nkd-phpmyadmin phpmyadmin
