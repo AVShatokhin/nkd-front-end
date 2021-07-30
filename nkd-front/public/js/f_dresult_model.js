a83eb5d694f8b1d06b89e033b9dd51ff871c37c1
AVShatokhin <andrey.v.shatokhin@gmail.com>
2021-07-30 10:10:24 +0300

    +иконки в дерево диагностики

--- nkd-front/public/js/f_dresult_model.js
+++ nkd-front/public/js/f_dresult_model.js
@@ -95,11 +95,7 @@ class dresult_model {
       if (__yes > 0) __badge_type = "bg-danger";
 
       return this._container(
-        this._badge(
-          `${__yes} / ${__suspicion}`,
-          // `Обнаружено : ${__yes}; Под подозрением : ${__suspicion}`,
-          __badge_type
-        )
+        this._badge(`${__yes} / ${__suspicion}`, __badge_type)
       );
     } else {
       return this._container("Нет данных");
@@ -109,7 +105,7 @@ class dresult_model {
   getSubListData(uuid, sid, slid) {
     if (this._subListData?.[`{${uuid}}`]?.[sid]?.[slid]) {
       let __cell = this._subListData?.[`{${uuid}}`]?.[sid]?.[slid];
-      return this._container(__cell.main);
+      return this._container(this._icons(__cell.main));
     } else {
       return this._container("-");
     }
@@ -178,5 +174,63 @@ class dresult_model {
 
   _badge(content, type) {
     return `<span class="badge ${type} p-2">${content}</span>`;
+  }
+
+  _icons(index) {
+    let __icons = {
+      no: `<div class="m-0 p-0"><svg id="Слой_1" data-name="Слой 1" xmlns="http://www.w3.org/2000/svg" height="14" viewBox="0 0 16 16">
+        <defs>
+          <style>
+            .cls-1 {
+              fill: #ccc;
+            }
+          </style>
+        </defs>
+        <g>
+          <path class="cls-1" d="M541.39,526l1.36,1a1,1,0,0,1,.25,1.41h0l-8.17,12.54a1.6,1.6,0,0,1-2.28.45l-.33-.23a1.61,1.61,0,0,1-.41-2.19l8.26-12.67a.93.93,0,0,1,1.32-.26Z" transform="translate(-527.17 -525.85)"/>
+          <path class="cls-1" d="M527.44,534.83l1.1-1.14a.9.9,0,0,1,1.31,0h0l4.72,4.91a1.91,1.91,0,0,1,0,2.63l-.21.22a1.29,1.29,0,0,1-1.86,0l-5.06-5.26a1,1,0,0,1,0-1.36Z" transform="translate(-527.17 -525.85)"/>
+        </g>
+      </svg></div>`,
+      yes: `<span class="m-0 p-1 badge bg-danger"><svg id="Слой_1" data-name="Слой 1" xmlns="http://www.w3.org/2000/svg" height="14" viewBox="0 0 40 16">
+        <defs>
+          <style>
+            .cls-1 {
+              fill: #ccc;
+            }
+          </style>
+        </defs>
+        <g>
+          <g>
+            <rect class="cls-1" y="3.29" width="15.31" height="12.71"/>
+            <polygon class="cls-1" points="17.96 6.56 15.28 9.68 15.27 7.96 15.29 3.3 17.96 6.56"/>
+            <polygon class="cls-1" points="17.94 12.91 15.29 15.99 15.26 14.28 15.26 9.67 17.94 12.91"/>
+          </g>
+          <g>
+            <polygon class="cls-1" points="31.02 6.39 31.02 6.39 31.02 6.39 31.02 6.39 31.02 6.39"/>
+            <polygon class="cls-1" points="31.02 6.39 31.02 6.39 31.02 6.39 31.02 6.39 31.02 6.39"/>
+            <polygon class="cls-1" points="31.02 6.39 31.02 6.39 31.02 6.39 31.02 6.39 31.02 6.39"/>
+          </g>
+          <g>
+            <rect class="cls-1" x="425.58" y="173.27" width="15.33" height="12.71" transform="translate(-401.3 -172.35) rotate(-0.12)"/>
+            <polygon class="cls-1" points="22.03 9.48 24.71 6.35 24.73 8.07 24.72 12.73 22.03 9.48"/>
+            <polygon class="cls-1" points="22.04 3.13 24.69 0.03 24.71 1.74 24.71 6.35 22.04 3.13"/>
+          </g>
+        </g>
+      </svg></span>`,
+      suspicion: `<span class="m-0 p-1 badge bg-warning"><svg id="Слой_1" data-name="Слой 1" xmlns="http://www.w3.org/2000/svg" height="14" viewBox="0 0 16 16">
+        <defs>
+          <style>
+            .cls-1 {
+              fill: #ccc;
+            }
+          </style>
+        </defs>
+        <g>
+          <path class="cls-1" d="M104.4,403h-1.74a.82.82,0,0,1-.81-.84h0l-.19-10.05c0-.46.55-.58,1-.58h1.74c.45,0,1,.2,1,.66l-.21,10a.82.82,0,0,1-.81.84Z" transform="translate(-101.5 -391.5)"/>
+          <path class="cls-1" d="M102.8,404.12h1.4a1.32,1.32,0,0,1,1.3,1.35v.68a1.33,1.33,0,0,1-1.3,1.35h-1.4a1.33,1.33,0,0,1-1.3-1.35v-.68A1.32,1.32,0,0,1,102.8,404.12Z" transform="translate(-101.5 -391.5)"/>
+        </g>
+      </svg></span>`,
+    };
+    return __icons[index];
   }
 }
